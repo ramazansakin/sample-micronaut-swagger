0080dcf9431fc6cbec1eb41544424b1f60d0bf19
ramazansakin <ramazansakin63@gmail.com>
2023-12-15 14:38:36 +0300

    feat: sample greetings endpoint with swagger doc defined

--- src/main/java/com/rsakin/Application.java
+++ src/main/java/com/rsakin/Application.java
@@ -1,14 +1,20 @@
 package com.rsakin;
 
 import io.micronaut.runtime.Micronaut;
-import io.swagger.v3.oas.annotations.*;
-import io.swagger.v3.oas.annotations.info.*;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @OpenAPIDefinition(
-    info = @Info(
-            title = "sample-micronaut-swagger",
-            version = "0.0"
-    )
+        info = @Info(
+                title = "Sample Micronaut Swagger",
+                version = "1.0.0",
+                description = "Sample PoC Micronaut App to integrate Swagger and Customize the API Docs",
+                license = @License(name = "Test", url = ""),
+                contact = @Contact(url = "https://github.com/ramazansakin/sample-micronaut-swagger",
+                        name = "Ramazan Sakin", email = "ramazansakin63@gmail.com")
+        )
 )
 public class Application {
 
--- src/main/java/com/rsakin/SampleMicronautSwaggerController.java
+++ src/main/java/com/rsakin/SampleMicronautSwaggerController.java
@@ -1,12 +1,30 @@
 package com.rsakin;
 
-import io.micronaut.http.annotation.*;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Controller("/sample-micronaut-swagger")
 public class SampleMicronautSwaggerController {
 
-    @Get(uri="/", produces="text/plain")
-    public String index() {
-        return "Example Response";
+    @Get(uri = "/", produces = "text/plain")
+    @Operation(summary = "Greets a person",
+            description = "A friendly greeting is returned"
+    )
+    @ApiResponse(
+            content = @Content(mediaType = "text/plain",
+                    schema = @Schema(type = "string"))
+    )
+    @ApiResponse(responseCode = "400", description = "Invalid Name Supplied")
+    @ApiResponse(responseCode = "404", description = "Person not found")
+    @Tag(name = "greeting")
+    public String greetings(@Parameter(description = "The name of the person") String name) {
+        return "Hello " + name + ", How are you doing?";
     }
+
 }
